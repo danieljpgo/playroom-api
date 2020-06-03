7346588729fb119c5a7d038f4762d2b6f27504ab
Daniel Jorge <djpgonzaga@gmail.com>
2020-06-03 00:14:36 -0300

    feature(database): add knex, sqlite3 and routes file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Teste');
-
-  response.json([
-    'Josiane',
-    'Daniel'
-  ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
