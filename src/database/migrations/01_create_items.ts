5df0335f19292a89a82d84c57330d1d236e0d722
Daniel Jorge <djpgonzaga@gmail.com>
2020-06-03 01:17:36 -0300

    feature(database): finish of migrations sqlite files

--- src/database/migrations/01_create_items.ts
+++ src/database/migrations/01_create_items.ts
@@ -1,7 +1,13 @@
-export async function up() {
+import Knex from 'knex';
 
+export async function up(knex: Knex) {
+  return knex.schema.createTable('items', table => {
+    table.increments('id').primary();
+    table.string('title').notNullable();
+    table.string('image').notNullable();
+  });
 }
 
-export async function down() {
-
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('items');
 }
