fee5a32f64013becafe3444fa0ee5b72097d1b27
Daniel Jorge <djpgonzaga@gmail.com>
2020-06-03 21:30:04 -0300

    feature(database): add seeds and images for items

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import knex from './database/connection';
+import { Item } from './models/item';
+
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+const port = '3333'
+const adress = `http://localhost:${port}`
+
+routes.get('/items', async (request, response) => {
+  const items: Item[] = await knex('items').select('*');
+
+  const serializedItems = items.map(item => ({
+    id: item.id,
+    title: item.title,
+    image_url: `${adress}/uploads/${item.image}`
+  }));
+
+  return response.json(serializedItems);
 });
 
 export default routes;
