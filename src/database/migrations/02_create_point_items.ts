737cdb4878e8ee1f22242d663bd61f9a906ba66a
Daniel Jorge <djpgonzaga@gmail.com>
2020-06-04 21:34:05 -0300

    fix(package.json): add flags to reload project

--- src/database/migrations/02_create_point_items.ts
+++ src/database/migrations/02_create_point_items.ts
@@ -4,12 +4,10 @@ export async function up(knex: Knex) {
   return knex.schema.createTable('point_items', table => {
     table.increments('id')
       .primary();
-
     table.integer('point_id')
       .notNullable()
       .references('id')
       .inTable('points');
-
     table.integer('item_id')
       .notNullable()
       .references('id')
