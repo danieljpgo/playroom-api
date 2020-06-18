8b208edadb2484ed5497c7e85248a8bd631bbd8d
Daniel Jorge <djpgonzaga@gmail.com>
2020-06-17 21:43:30 -0300

    feature(excess-treatment): add joi to excess treatment

--- src/api/points/index.ts
+++ src/api/points/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
 import multer from 'multer';
 import multerConfig from '../../services/multer/index';
 import { index, show, create } from './controller';
@@ -8,6 +9,21 @@ const upload = multer(multerConfig);
 
 router.get('/', index);
 router.get('/:id', show);
-router.post('/', upload.single('image'), create);
+router.post('/',
+  upload.single('image'), // @TODO validar a image
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      state: Joi.string().required().max(2),
+      items: Joi.string().required(), // @TODO validate number with regex
+    })
+  }, { abortEarly: false }), // @TODO move to model file
+  create
+);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from './services/express';
 import api from './api';
 import config from './config';
+import { errors } from 'celebrate';
 
 const { port } = config;
 
 const app = express(api);
+
+app.use(errors());
 
 app.listen(port);
